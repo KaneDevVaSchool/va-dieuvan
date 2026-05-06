3d92b4bc53b418898ec9feb91e80a26485076b01
Nguyễn Khoa <134806583+ResumeKNTech@users.noreply.github.com>
2026-05-06 14:19:38 +0700

    a

--- resources/js/src/views/requests/dispatch-wizard/DispatchWizardStep3.vue
+++ resources/js/src/views/requests/dispatch-wizard/DispatchWizardStep3.vue
@@ -6,23 +6,6 @@
       </h2>
     </div>
 
-    <div class="mb-6 rounded-xl border border-slate-200 bg-slate-50/80 p-4 sm:p-5">
-      <label class="block">
-        <span class="dw-label-text">{{ t('dispatch_wizard.s3.readonly_depart_label') }}</span>
-        <input
-          :value="formattedRequestedDateTime"
-          type="text"
-          readonly
-          tabindex="-1"
-          :placeholder="t('dispatch_wizard.s3.readonly_depart_empty')"
-          class="dw-input mt-1 min-h-[2.75rem] cursor-not-allowed bg-slate-100 text-slate-800"
-        />
-        <span class="mt-1 block text-xs text-slate-500">
-          {{ t('dispatch_wizard.s3.readonly_depart_hint', { step: t('dispatch_wizard.steps.info') }) }}
-        </span>
-      </label>
-    </div>
-
     <!-- Hành khách / door / p2p: e.1 -->
     <div v-if="!isCargo" class="dw-step3-stack">
       <section v-if="showE1Schedule" class="dw-step3-section" aria-labelledby="dw-e1-heading">
@@ -393,7 +376,6 @@ const {
   openDatePickerFromInput,
   toggleE1Weekday,
   detailStepSchedulesValid,
-  formattedRequestedDateTime,
 } = w
 
 const schedulePassengerValid = ref(true)
