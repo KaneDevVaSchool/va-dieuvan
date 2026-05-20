7e8aefe5fdf4f2f08e8854e6806cf440234d1ba0
Nguyễn Khoa <134806583+ResumeKNTech@users.noreply.github.com>
2026-05-20 11:42:36 +0700

    a

--- resources/js/src/App.vue
+++ resources/js/src/App.vue
@@ -4,7 +4,6 @@
     :app-ready="splashAppReady"
     @done="showSplash = false"
   />
-  <PwaUpdateBanner />
   <Onboarding
     v-if="onboardingViewportOk && !hasOnboarded && isAuthenticated && !isPortalShell && !isDeptShell"
     @done="completeOnboarding"
@@ -40,8 +39,6 @@ import SplashScreen from './components/SplashScreen.vue'
 import Onboarding from './components/Onboarding.vue'
 import NotificationCenter from './components/notifications/NotificationCenter.vue'
 import NotificationToast from './components/notifications/NotificationToast.vue'
-import PwaUpdateBanner from './components/pwa/PwaUpdateBanner.vue'
-
 const route = useRoute()
 const auth = useAuthStore()
 const { hasOnboarded, completeOnboarding } = useOnboarding()
